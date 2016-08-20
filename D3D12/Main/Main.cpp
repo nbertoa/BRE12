e5c2ded708df3deb17f59afc426b7a63c3314a6e
Nicolas Bertoa <nicolas.bertoa@outlook.com>
2016-08-20 01:57:33 -0300

    .

--- D3D12/Main/Main.cpp
+++ D3D12/Main/Main.cpp
@@ -1,10 +1,7 @@
-#include <memory>           
 #include <windows.h>
 
 #include <App/App.h>
 #include <BasicScene/BasicScene.h>
-
-#include <tbb\tbb_thread.h>
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE /*hPrevInstance*/, _In_ LPSTR /*lpCmdLine*/, _In_ int /*nShowCmd*/) {
 	BasicScene scene;
