1ec0a2a8b13707beb2e52c27674df09ad99c348c
Nicolas Bertoa <nicolas.bertoa@outlook.com>
2017-06-23 01:11:39 -0300

    Added more scenes and materials

--- BRE/Main/Main.cpp
+++ BRE/Main/Main.cpp
@@ -97,7 +97,7 @@ WINAPI WinMain(_In_ HINSTANCE moduleInstanceHandle,
                _In_ LPSTR /*commandLine*/,
                _In_ int /*showCommand*/)
 {
-    const char* sceneFilePath = "resources/scenes/rock.yml";
+    const char* sceneFilePath = "resources/scenes/fabric.yml";
 
     tbb::task_scheduler_init taskSchedulerInit;
 
